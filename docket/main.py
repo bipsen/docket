a15a15aa7853b26e468bb371336f6c8b2e77e307
bipsen <ipsen.bertil@gmail.com>
2022-09-14 13:40:51 +0200

    Changed to module-level logging

--- docket/main.py
+++ docket/main.py
@@ -4,9 +4,9 @@ import logging
 from pyairtable import Table
 from pyairtable.formulas import match
 
-logging.basicConfig(
-    format="%(asctime)s | %(levelname)s: %(message)s", level=logging.INFO
-)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class Docket:
